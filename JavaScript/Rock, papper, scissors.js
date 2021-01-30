8efeffcec812e8502cee1c83c568ba86e4a88254
nubis <yo@nubis.im>
2021-01-30 14:15:42 -0300

    Adds tests

--- JavaScript/Rock, papper, scissors.js
+++ JavaScript/Rock, papper, scissors.js
@@ -1,57 +1,80 @@
-const getUserChoice = userInput => {
-  userInput = userInput.toLowerCase();
-  if (userInput === "rock" || userInput === "paper" || userInput === "scissors" || userInput ==="bomb") {
-    return userInput;
-  } else {
-    console.log("Error!");
-  }
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+   
+const choices = { 
+  "rock": "scissors",
+  "paper": "rock",
+  "scissors": "paper"
+};
+
+const sanitizeUserChoice = userInput => {
+  let sanitized = userInput.toLowerCase();
+  let options = Object.keys(choices).concat(["bomb"]);
+  return options.includes(sanitized) ? sanitized : null;
 };
 
 const getComputerChoice = () => {
-const randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
+  return Object.keys(choices)[Math.floor(Math.random() * 3)];
 }
 
-const determineWinner = (userChoice,computerChoice) => {
-  if (userChoice === "bomb") {
-    return "you are unbeatable winner"
-  };
-  if (userChoice === computerChoice){
-    return "That is a tie!";
-  };
-  if (userChoice === "rock") {
-    if (computerChoice === "paper") {
-   	return "Computer won";
-  } else {
-    return "User won";
-  }
-  }
-  if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-   	return "Computer won";
-  } else {
-    return "User won";
-  }
-  }
-  if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-   	return "Computer won";
-  } else {
-    return "User won";
-  }
+const determineWinner = (userChoice, computerChoice) => {
+  switch (userChoice) {
+    case "bomb":
+      return "won";
+    case computerChoice:
+      return "tie";
+    default:
+      return (choices[userChoice] == computerChoice) ? "won" : "lost";
   }
 }
 
 const playGame = () => {
-  console.log(`player chose ${getUserChoice('')}`);
-  console.log(`computer chose ${getComputerChoice()}`);
-  console.log(determineWinner(getUserChoice(""),getComputerChoice()));
+  readline.question('Choose rock, paper, scissors. "bye" to quit:', choice => {
+    if (choice == 'bye') {
+      console.log('Ok, bye')
+      process.exit(0);
+    }
+
+    let user = sanitizeUserChoice(choice);
+
+    if(!user){
+      console.log("Unknown choice:", choice);
+      return playGame();
+    }
+
+    readline.close();
+
+    let computer = getComputerChoice();
+
+    console.log(`User: ${user}. Computer: ${computer}`);
+
+    let result = determineWinner(user, computer);
+
+    console.log({
+      "won": "User won",
+      "tie": "That is a tie",
+      "lost": "Computer Won"
+    }[result]);
+  });
 }
-playGame('rock');
+
+function run_tests(){
+  var assert = require('assert');
+  assert(determineWinner("rock", "paper") == "lost");
+  assert(determineWinner("paper", "rock") == "won");
+  assert(determineWinner("rock", "scissors") == "won");
+  assert(determineWinner("paper", "paper") == "tie");
+  assert(determineWinner("rock", "rock") == "tie");
+  assert(determineWinner("bomb", "paper") == "won");
+  assert(determineWinner("bomb", "scissors") == "won");
+  assert(sanitizeUserChoice("rock") == "rock");
+  assert(sanitizeUserChoice("PapeR") == "paper");
+  assert(sanitizeUserChoice("bogus") == null);
+  console.log("Todos los tests salieron bien");
+}
+
+run_tests()
+playGame();
+
